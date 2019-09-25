05070d6e64b72fe0ab6018c2b58846945f0b3966
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-25 15:36:56 +0000

    lunch

--- src/app/features/gift-giving/containers/friend-gifts/friend-gifts.component.ts
+++ src/app/features/gift-giving/containers/friend-gifts/friend-gifts.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { map, tap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
-import { GiftGivingState } from '../../reducers';
+import { Subscription, Observable } from 'rxjs';
+import { GiftGivingState, selectFriendHolidayModel } from '../../reducers';
 import { Store } from '@ngrx/store';
 import { setSelectedFriend } from '../../actions/friend.actions';
+import { FriendHoliday } from '../../models';
+import { assignHolidayToFriend } from '../../actions/friend-holiday.actions';
 
 @Component({
   selector: 'app-friend-gifts',
@@ -12,18 +14,19 @@ import { setSelectedFriend } from '../../actions/friend.actions';
   styleUrls: ['./friend-gifts.component.css']
 })
 export class FriendGiftsComponent implements OnInit, OnDestroy {
-  id: string;
+
+  model$: Observable<FriendHoliday>;
   subscription: Subscription;
 
   constructor(private router: Router,
     private activatedRoute: ActivatedRoute,
     private store: Store<GiftGivingState>) { }
   ngOnInit() {
-
+    this.model$ = this.store.select(selectFriendHolidayModel);
     this.subscription = this.activatedRoute.paramMap.pipe(
       map(params => {
-        this.id = params.get('id');
-        this.store.dispatch(setSelectedFriend({ id: this.id }));
+        // this.id = params.get('id');
+        this.store.dispatch(setSelectedFriend({ id: params.get('id') }));
       }),
       tap(() => console.log('Is this thing on?'))
     ).subscribe();
@@ -31,6 +34,10 @@ export class FriendGiftsComponent implements OnInit, OnDestroy {
 
   backToList() {
     this.router.navigate(['/gifts', 'friends']);
+  }
+
+  addHoliday(friendId: string, holidayId: string) {
+    this.store.dispatch(assignHolidayToFriend({ friendId, holidayId }));
   }
 
   ngOnDestroy() {
