4d3a72526ceb5aa2f1bb5c2946fb4c1d7715c6c2
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-25 19:19:32 +0000

    before pwa

---
+++ src/app/features/admin/admin.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent
+  }
+];
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [AdminComponent]
+})
+export class AdminModule { }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,11 +1,26 @@
 import { RouterState, routerReducer } from '@ngrx/router-store';
-
+import * as fromAuth from './auth.reducer';
+import { createSelector } from '@ngrx/store';
 export interface AppState {
 
   router: RouterState.Minimal;
+  auth: fromAuth.AuthState;
 }
 
 
 export const reducers = {
-  router: routerReducer
+  router: routerReducer,
+  auth: fromAuth.reducer
 };
+
+
+// Selectors
+
+// 1. No "feature selector" because we ARE the app
+
+// 2. Selector per branch
+
+export const selectAuthBranch = (state: AppState) => state.auth;
+// 3. For the components.
+export const selectIsAdmin = createSelector(selectAuthBranch, a => a.isAdmin);
+export const selectUserName = createSelector(selectAuthBranch, a => a.userName);
